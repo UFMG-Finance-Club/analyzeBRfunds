017251ebc9b3ded953a437d8ee6ee90599e1b5bb
Marcel <marcel.zanetti@cinnecta.com>
2022-03-08 20:14:25 -0300

    add downloaders for funds and ibov

--- utils/DataDownload.py
+++ utils/DataDownload.py
@@ -1,9 +1,11 @@
 import requests
+import pandas as pd
+import pandas_datareader as pdr
 import datetime
+import os
 from collections import OrderedDict
-from typing import List
+from typing import List, Union
 
-# adicionar checagem se o diretório existe
 def get_months_interval(months_interval: List[datetime.date]) -> List[str]:
     """Return list of months between a given interval. You should not need to evoke this directly.
     
@@ -20,26 +22,23 @@ def get_months_interval(months_interval: List[datetime.date]) -> List[str]:
     )
     return list(months_download)
 
-def download_data(first_date: List[int], last_date: List[int], outpath: str) -> List[str]:
-    """Download funds' data. Designed for downloading from CVM. It returns a list with names of the downloaded names, so you can
+# DOWNLOAD FUNDS DATA
+def download_funds(months_interval: List[datetime.date], outpath: str) -> List[str]:
+    """Download and write funds' data. Designed for downloading from CVM. It returns a list with names of the downloaded names, so you can
     use it as an argument in the preprocessing routine.  
 
-        :param first_date: list with first year and month
-        :param last_date: list with last year and month
-        :param outpath: (local) path to save downloaded data
+    :param months_interval: list with date objects of first and last month/year
+    :param outpath: (local) path to save downloaded data
     """
 
     inpath_structure = "http://dados.cvm.gov.br/dados/FI/DOC/INF_DIARIO/DADOS/inf_diario_fi_"
 
     # GETTING ALL MONTHS IN THE INTERVAL
-    months_interval = [
-        datetime.date(first_date[0], first_date[1], 1), datetime.date(last_date[0], last_date[1], 2)
-    ]
     months_download = get_months_interval(months_interval)
 
     # GENERATING FILE NAMES
     input_file_names = [f"{inpath_structure}{d}.csv" for d in months_download]
-    output_file_names = [f"{outpath}/{d}.csv" for d in months_download]
+    output_file_names = [os.path.join(outpath, f"{d}.csv") for d in months_download]
 
     # DOWNLOAD FILES
 
@@ -48,3 +47,50 @@ def download_data(first_date: List[int], last_date: List[int], outpath: str) -> 
         open(output_file_names[i], "wb").write(r.content)
 
     return output_file_names
+
+# DOWNLOAD IBOV
+def download_ibov(date_interval: List[datetime.date]) -> pd.DataFrame:
+    """Download IBOV data
+    
+    :param date_interval: date interval to download data
+    """
+
+    data = (
+        pdr.data.DataReader("^BVSP", data_source="yahoo", start=date_interval[0], end=date_interval[1])
+        .reset_index()
+    )[["Date", "Adj Close"]]
+    data.columns = ["Date", "Value"]
+    data["Name"] = "IBOV"
+
+    return data
+
+# DOWNLOAD DATA - GENERAL
+def download_data(
+    first_date: List[int], last_date: List[int], 
+    asset: Union[str, List[str]] = "FUNDS", outpath: str = None
+) -> None:
+    """General function to download supported data
+    
+    :param first_date: list with first year and month
+    :param last_date: list with last year and month
+    :param asset: which asset data to download
+    :param outpath: (local) path to save downloaded data
+    """
+
+    correspondence_asset_function = {
+        "FUNDS" : download_funds,
+        "IBOVESPA" : download_ibov,
+        "RISK-FREE" : download_riskfree
+    }
+
+    first_day = first_date[2] if len(first_date) == 2 else 1
+    last_day = last_date[2] if len(last_date) == 2 else 1
+    date_interval = [
+        datetime.date(first_date[0], first_date[1], first_day), 
+        datetime.date(last_date[0], last_date[1], last_day)
+    ]
+
+    if outpath:
+        correspondence_asset_function[asset](date_interval, outpath)
+    else:
+        correspondence_asset_function[asset](date_interval)
