a49573fc2df5a3e86b40ec8e4296f19392e3cb4d
Marcel <marcel.zanetti@cinnecta.com>
2022-03-09 13:25:22 -0300

    finish get_return routine

--- metrics/FinancialMetrics.py
+++ metrics/FinancialMetrics.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import datetime
-import os
+from ..utils import DataDownload
 from typing import List, Dict
 
 
@@ -31,7 +31,11 @@ class PerformanceMetrics():
             self.data = self.data[self.data["Date"] <= datetime.date(last_date[0], last_date[1], 31)]
 
         # ADJUSTING OTHER PARAMS
+        self.update_correspondence()
         self.returns_data = None
+
+    def update_correspondence(self) -> None:
+        self.correspondence = self.data[["Asset", "Name"]].drop_duplicates()
 
     def increment_with(self, inpath_bases: Dict[str, str]):
         """Increment data with external sources
@@ -53,14 +57,16 @@ class PerformanceMetrics():
         # IF RETURN DATA WAS ALREADY CALCULATED, ASK TO UPDATE IT
         if self.returns_data:
             self.get_returns()
+        self.update_correspondence()
 
     def get_returns(self):
         """Compute returns
+
         """
-        if not self.returns_data:
-            self.returns_data = (
-                self.data
-                .pivot(index="DT_COMPTC", columns="CNPJ", values="VL_QUOTA")
-                .dropna(axis=1)
-                .pct_change()
-            )
+        self.returns_data = (
+            self.data
+            .drop("Name", axis=1)
+            .pivot(index="Date", columns="Asset", values="Value")
+            .dropna(axis=1)
+            .pct_change()
+        )
